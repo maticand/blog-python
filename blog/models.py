9257d3c08996fd54cc1d9f266d61c3a5a2b352cb
Matias Candia <matiasclaudiocandia@gmail.com>
2019-09-19 18:31:11 -0300

    Aplicación Django Blog

---
+++ blog/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.utils import timezone
+
+class Post(models.Model):
+    autor = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    titulo = models.CharField(max_length=200)
+    texto = models.TextField()
+    fecha_creado = models.DateTimeField(default=timezone.now)
+    fecha_publicado = models.DateTimeField( blank=True, null=True)
+
+    # Método Publicar
+    def publicar(self):
+        self.flecha_publicado = timezone.now()
+        self.save()
+
+    def  __str__(self):
+        return self.titulo
