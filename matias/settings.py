09fc500e34a23fd6c3fe911fc8494a0592515d09
Matias Candia <matiasclaudiocandia@gmail.com>
2019-09-22 16:22:04 -0300

    Update settings.py

--- matias/settings.py
+++ matias/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, 'static'),
 ]
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
