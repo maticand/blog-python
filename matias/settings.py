90f2e3d0236fca5c41ece981f6956104aa6de3d9
Matias Candia <matiasclaudiocandia@gmail.com>
2019-09-22 17:00:00 -0300

    Update settings.py

--- matias/settings.py
+++ matias/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+STATIC_ROOT = '/home/matias/CodeProjects/PythonProjects/matias'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
