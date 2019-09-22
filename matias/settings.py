b6f79c4fa99415bb884b4af111d85725c6a0f4c8
Matias Candia <matiasclaudiocandia@gmail.com>
2019-09-22 15:15:07 -0300

    Aplicación Django Blog

--- matias/settings.py
+++ matias/settings.py
@@ -125,9 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-)
+]
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
