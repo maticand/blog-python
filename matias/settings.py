aaf03cd0e0acc3c92b1ccc5975f13126ca3af99e
Matias Candia <matiasclaudiocandia@gmail.com>
2019-09-24 19:53:21 -0300

    Aplicación Django Blog

--- matias/settings.py
+++ matias/settings.py
@@ -124,16 +124,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = '/home/matias/CodeProjects/PythonProjects/matias/'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-STATICFILES_FINDERS = [
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-]
-
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
