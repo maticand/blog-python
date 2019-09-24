a396f6a57d0040b9d1a5be8f372d1e5fe01094a6
Matias Candia <matiasclaudiocandia@gmail.com>
2019-09-24 20:18:33 -0300

    Aplicación Django Blog

--- matias/settings.py
+++ matias/settings.py
@@ -128,6 +128,11 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
