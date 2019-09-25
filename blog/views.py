eb5a9ff04c39b266938bac530e5a83bf813564fe
Matias Candia <matiasclaudiocandia@gmail.com>
2019-09-24 21:19:01 -0300

    Aplicación Django Blog

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 # Django
 from django.shortcuts import render
+from .forms import PostForm
+from django.shortcuts import redirect
 
 # Utilidades
 from django.utils import timezone
@@ -19,3 +21,32 @@ def post_feed(request):
 def post_detalle(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detalle.html', {'post': post})
+
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.autor = request.user
+            post.fecha_publicado = timezone.now()
+            post.save()
+            return redirect('post_detalle', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.autor = request.user
+            post.fecha_publicado = timezone.now()
+            post.save()
+            return redirect('post_detalle', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
