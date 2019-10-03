6d7d5e5c1c861f403a62ac001d3c03f19eaccd92
Matias Candia <matiasclaudiocandia@gmail.com>
2019-10-02 23:22:00 -0300

    Creación de Modelos

--- aplicaciones/blog/models.py
+++ aplicaciones/blog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+class Categoria(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField('Nombre de la Categoría', max_length=100, null=False, blank=False)
+    estado = models.BooleanField('Activo/No Activo', default=True)
+    fecha_creacion = models.DateField('Fecha de Creación', auto_now=False, auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Categoría'
+        verbose_name_plural = 'Categorías'
+
+    def __str__(self):
+        return self.nombre
+
+
+class Autor(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField('Nombre de Autor', max_length=100, null=False, blank=False)
+    apellido = models.CharField('Apellido de Autor', max_length=100, null=False, blank=False)
+    facebook = models.URLField('Facebook', null=True, blank=True)
+    twitter = models.URLField('Twitter', null=True, blank=True)
+    instagram = models.URLField('Instagram', null=True, blank=True)
+    website = models.URLField('Website', null=True, blank=True)
+    correo = models.EmailField('Correo Electrónico', blank=False, null=False)
+    estado = models.BooleanField('Activo/No Activo', default=True)
+    fecha_creacion = models.DateField('Fecha de Creación', auto_now=False, auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Autor'
+        verbose_name_plural = 'Autores'
+
+    def __str__(self):
+        return "{0},{1}".format(self.apellido, self.nombre)
--- matias/settings.py
+++ matias/settings.py
@@ -57,7 +57,9 @@ ROOT_URLCONF = 'matias.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
