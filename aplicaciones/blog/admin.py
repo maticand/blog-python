0ccf446cfbb48682be6f6cee3a8e3891625e8c8f
Matias Candia <matiasclaudiocandia@gmail.com>
2019-10-02 23:29:05 -0300

    Personalizando Vista de Modelos en Admin de Django

--- aplicaciones/blog/admin.py
+++ aplicaciones/blog/admin.py
@@ -2,5 +2,15 @@ from django.contrib import admin
 from .models import Categoria
 from .models import Autor
 
-admin.site.register(Categoria)
-admin.site.register(Autor)
+class CategoriaAdmin(admin.ModelAdmin):
+    search_fields = ['nombre']
+    list_display = ('nombre','estado','fecha_creacion',)
+    list_filter = ('fecha_creacion',)
+
+class AutorAdmin(admin.ModelAdmin):
+    search_fields = ['nombre','apellido','correo']
+    list_display = ('nombre', 'apellido', 'correo', 'estado', 'fecha_creacion',)
+    list_filter = ('nombre','apellido','correo','fecha_creacion',)
+
+admin.site.register(Categoria, CategoriaAdmin)
+admin.site.register(Autor, AutorAdmin) 
