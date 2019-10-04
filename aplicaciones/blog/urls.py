58a9b4f0999ca1818d5782ea8d307395411bf2e5
Matias Candia <matiasclaudiocandia@gmail.com>
2019-10-04 20:41:02 -0300

    Listado de Post

--- aplicaciones/blog/urls.py
+++ aplicaciones/blog/urls.py
@@ -5,8 +5,10 @@ from . import views
 urlpatterns = [
     path('amp', views.amp, name='amp'),
     path('', views.index, name='index'),
-    path('feed', views.feed, name='feed'),
-    path('generales', views.generales, name='generales'),
+    path('sobre-mi', views.sobremi, name='sobremi'),
+    path('about', views.about, name='about'),
+
+
     # path('post/<int:pk>/', views.post_detalle, name='post_detalle'),
     # path('post/new', views.post_new, name='post_new'),
     # path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
--- aplicaciones/blog/views.py
+++ aplicaciones/blog/views.py
@@ -1,18 +1,21 @@
 from django.shortcuts import render
 from .models import Post, Categoria
 
+def amp(request):
+    return render(request,'index.amp.html')
+
+
 def index(request):
     posts = Post.objects.filter(estado = True).order_by('fecha_publicado')
     return render(request,'index.html',{'posts':posts})
 
-def amp(request):
-    return render(request,'index.amp.html')
 
-def feed(request):
-    return render(request,'feed.html')
-
-def generales(request):
+def sobremi(request):
     posts = Post.objects.filter(
     estado = True,
     categoria = Categoria.objects.get(nombre = 'seo'))
-    return render(request,'generales.html', {'posts':posts})
+    return render(request,'blog/sobre-mi.html', {'posts':posts})
+
+
+def about(request):
+    return render(request,'blog/about.html')
