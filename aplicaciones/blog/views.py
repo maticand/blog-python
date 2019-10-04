16a62dedce9d3f76e018f33264ad7616c4ead361
Matias Candia <matiasclaudiocandia@gmail.com>
2019-10-03 22:10:08 -0300

    Sistema de Plantillas y Categorias en Templates

--- aplicaciones/blog/views.py
+++ aplicaciones/blog/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 
 def index(request):
+    return render(request,'index.html')
+
+def amp(request):
     return render(request,'index.amp.html')
+
+def feed(request):
+    return render(request,'feed.html')
