47c50bc1dd1767c8dfbcdb1ef741f4eb4a035949
Matias Candia <matiasclaudiocandia@gmail.com>
2019-10-09 00:16:21 -0300

    Preparando Host para deploy a Heroku

--- matias/settings/local.py
+++ matias/settings/local.py
@@ -3,7 +3,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
--- matias/settings/production.py
+++ matias/settings/production.py
@@ -2,7 +2,7 @@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['matiasblog.herokuapp.com']
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
