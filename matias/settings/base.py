a1d13abeb894d2a331b84e17402c4ce1f8b07e4e
Matias Candia <matiasclaudiocandia@gmail.com>
2019-10-08 23:58:12 -0300

    Preparando desplegue a Heroku

--- matias/settings/base.py
+++ matias/settings/base.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1^+gcv1h%lhtdf9oaee4cofvg0#tu(8_83==25ole^mgrxjk39'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -77,17 +77,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'matias.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 
 # Password validation
---
+++ matias/settings/production.py
@@ -0,0 +1,15 @@
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
