1038c114d9ae916f70e7c01e301d884e32cfed01
Matias Candia <matiasclaudiocandia@gmail.com>
2019-10-10 17:11:12 -0300

    Deploy a Heroku

--- matias/settings/production.py
+++ matias/settings/production.py
@@ -19,11 +19,4 @@ DATABASES = {
     }
 }
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-STATICFILES_FINDERS = [
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-]
+STATICFILES_DIRS = (BASE_DIR, 'static')
--- matias/wsgi.py
+++ matias/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'matias.settings.production')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'matias.settings.local')
 
 # application = get_wsgi_application()
 
