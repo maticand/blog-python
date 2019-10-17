95f1cc098fcd60374903a9b6c309e4d1945321f0
Matias Candia <matiasclaudiocandia@gmail.com>
2019-10-17 18:01:12 -0300

    Deploy a Heroku

--- manage.py
+++ manage.py
@@ -5,8 +5,8 @@ import sys
 
 
 def main():
+    # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'matias.settings.local')
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'matias.settings.production')
-    # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'matias.settings.production')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- matias/wsgi.py
+++ matias/wsgi.py
@@ -11,8 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'matias.settings.local')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'matias.settings.production')
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'matias.settings.production')
 
 # application = get_wsgi_application()
 
